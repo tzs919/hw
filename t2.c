95dd5f095ad5bd6d5565a3affa13136c0bdced7b
tzs919 <tzs919@163.com>
2020-05-18 22:49:37 +0800

    x

--- t2.c
+++ t2.c
@@ -2,10 +2,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "t1.h"
 
 int main(int argc, char *argv[])
+{
+    char name[20] = "     ";
+    char *p = strtok(name, " ");
+    if (p == NULL)
+    {
+        printf("===%ld===\n",strlen(""));
+    }
+}
+
+int main2(int argc, char *argv[])
 {
     int x = atoi(argv[1]);
     int y = atoi(argv[2]);
@@ -17,4 +28,5 @@ int main(int argc, char *argv[])
                                                 .score = 99};
     stu = stu2;
     printf("name:%s\nage:%d\nscore:%d\n", stu.name, stu.age, stu.score);
+    return 0;
 }
